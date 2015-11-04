4d2d86546581a8bf2b8f8a290bbd4961d5da59ac
Daniel W <danweller18@gmail.com>
2015-11-03 22:26:18 -0500

    sending mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #disable sending email
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :test
+  end
 end
